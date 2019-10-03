6960b69555cad1de5069afb2fbfd202da4e0087f
RLZW <rulazw@gmail.com>
2019-10-03 16:05:57 -0500

    Cambios de Logo y Fondo

--- app/src/main/java/mx/itesm/perdifirulais/MainActivity.kt
+++ app/src/main/java/mx/itesm/perdifirulais/MainActivity.kt
@@ -5,21 +5,8 @@ import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import com.google.firebase.auth.FirebaseAuth
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.auth.FirebaseUser
 import android.widget.Toast
-import com.google.firebase.auth.AuthResult
-import com.google.android.gms.tasks.Task
-import androidx.annotation.NonNull
-import android.R.attr.password
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.view.View
-import android.widget.EditText
-import androidx.fragment.app.FragmentActivity
 import com.google.android.material.textfield.TextInputEditText
 
 
@@ -38,13 +25,13 @@ class MainActivity : AppCompatActivity() {
 
         btnIngresar.setOnClickListener {
             //Log.d("Email", etUsuario.text.toString())
-            if(etUsuario.toString().isEmpty() || etContraseña.toString().isEmpty()){
+            if(etMail.toString().isEmpty() || etPassword.toString().isEmpty()){
                 return@setOnClickListener
             }
             else{
-                val textUsuario = findViewById<View>(R.id.etUsuario) as TextInputEditText
+                val textUsuario = findViewById<View>(R.id.etMail) as TextInputEditText
                 val valueUsuario = textUsuario.text.toString()
-                val textContraseña = findViewById<View>(R.id.etContraseña) as TextInputEditText
+                val textContraseña = findViewById<View>(R.id.etPassword) as TextInputEditText
                 val valueContraseña = textContraseña.text.toString()
                 Log.d("Email", valueUsuario)
 
